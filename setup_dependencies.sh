85e33d5336e9950ccf40d6f5bf85825e2c0713f4
Adam Anderson <adama@fnal.gov>
2018-06-27 01:50:51 +0000

    Add some more dependencies to setup script.

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -1,14 +1,16 @@
 # check if sudo is used
 if [ "$(id -u)" != 0 ]; then
   echo 'Sorry, you need to run this script with sudo'
-  exit 1
 fi
 
-apt-get install -y smbus i2c-tools              # I2C
-apt-get install -y python-dev python-rpi        # GPIO python control
-apt-get install -y git                          # git
-apt-get install -y avahi-daemon                 # mDNS
-apt-get install -y emacs                        # in case of desperation
+apt-get install -y python-smbus python3-smbus i2c-tools       # I2C
+apt-get install -y python-dev python-rpi.gpio python3-dev python3-rpi.gpio  # GPIO python control
+apt-get install -y git                                        # git
+apt-get install -y avahi-daemon                               # mDNS
+apt-get install -y emacs ipython python3-ipython              # in case of desperation
+
+git clone http://github.com/adamanderson/python_tb6612fng
+git clone http://github.com/adamanderson/python_mma8451
 
 touch /boot/ssh                                 # enable ssh https://www.raspberrypi.org/documentation/remote-access/ssh/
 
