417207c339030deab4f0c3b1592177dbdd45cace
Adam Anderson <adama@fnal.gov>
2018-06-27 02:53:22 +0000

    Fix typos in frequency sweep.

--- measure/frequency_sweep.py
+++ measure/frequency_sweep.py
@@ -1,7 +1,7 @@
 import argparse as ap
 import numpy as np
 from python_tb6612fng.TB6612FNG import TB6612FNG
-from python_mma8451.record_accelerometer as MMA8451DAQ
+from python_mma8451.record_accelerometer import MMA8451DAQ
 
 P = ap.ArgumentParser(description='Perform frequency sweep to measure '
                       'mechanical transfer function.',
@@ -10,7 +10,7 @@ P.add_argument('voltage', type=float,
                help='Power supply voltage setting used for motor drive.')
 P.add_argument('motor', choices=['JQ24-35F580C'],
                help='Type of motor used for vibration.')
-P.add_argument('--motor-range', type=float, nargs=2, default=[0,100]
+P.add_argument('--motor-range', type=float, nargs=2, default=[0,100],
                help='Min and max of range of motor voltages to apply. Both '
                'upper and lower values must be between 0 and 100 inclusive.')
 P.add_argument('--n-voltages', type=int, default=10,
@@ -24,7 +24,7 @@ args = P.parse_args()
 if args.motor_range[0] < 0 or args.motor_range[0] > 100 or \
    args.motor_range[1] < 0 or args.motor_range[1] > 100:
     raise ValueError('Argument `--motor-range` out of allowed range.')
-   args.motor_range[0] > args.motor_range[1]:
+if args.motor_range[0] > args.motor_range[1]:
     raise ValueError('Upper end of `--motor-range` arugment is less than '
                      'lower end.')
 
