74f95510828b4565137997ee153a45a5538790d2
Adam Anderson <adama@fnal.gov>
2018-10-02 17:00:20 -0500

    Add git, mDNS, emacs, and ssh support.

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -4,5 +4,11 @@ if [ "$(id -u)" != 0 ]; then
   exit 1
 fi
 
-apt-get install -y smbus i2c-tools
-apt-get install -y python-dev python-rpi
+apt-get install -y smbus i2c-tools              # I2C
+apt-get install -y python-dev python-rpi        # GPIO python control
+apt-get install -y git                          # git
+apt-get install -y avahi-daemon                 # mDNS
+apt-get install -y emacs                        # in case of desperation
+
+touch /boot/ssh                                 # enable ssh https://www.raspberrypi.org/documentation/remote-access/ssh/
+
