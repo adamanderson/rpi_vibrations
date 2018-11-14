89501a2d2119d6ab1650ff07523e75a7636990ec
Adam Anderson <adama@fnal.gov>
2018-11-14 15:20:28 +1300

    Add command line option to run noise stares through GCP at pole during frequency sweep. Clean up handling of housekeeping files.

--- measure/frequency_sweep.py
+++ measure/frequency_sweep.py
@@ -5,6 +5,7 @@ from python_mma8451.record_accelerometer import MMA8451DAQ
 import pickle
 from datetime import datetime
 import time
+import os
 
 P = ap.ArgumentParser(description='Perform frequency sweep to measure '
                       'mechanical transfer function.',
@@ -21,6 +22,12 @@ P.add_argument('--n-voltages', type=int, default=10,
 P.add_argument('--duration', type=float, default=10,
                help='Duration of accelerometer data (in seconds) to take at '
                'each voltage step.')
+P.add_argument('--gcp-noise-stare', action='store_true',
+               help='Take a noise stare through GCP.')
+P.add_argument('--noise-stare-duration', type=float, default=60.0,
+               help='Duration of the GCP noise stare in seconds. Note that '
+               'a 1m noise stare takes 4m to execute because of associated '
+               'calibrator stare in the schedule.')
 args = P.parse_args()
 
 # check parameter ranges
@@ -33,6 +40,7 @@ if args.motor_range[0] > args.motor_range[1]:
 
 # housekeeping data
 hkdata = {}
+hkfilename = datetime.utcnow().strftime('%Y%m%d_%H%M%S_sweep_housekeeping.pkl')
 
 motor_ranges = np.linspace(args.motor_range[0], args.motor_range[1], args.n_voltages)
 motor = TB6612FNG()
@@ -41,7 +49,7 @@ motor.output_on(0)
 # take background measurment with motors off
 if 0.0 not in motor_ranges:
     motor_ranges = np.insert(motor_ranges, 0, 0)
-
+    
 # actually take the data
 for motor_range in motor_ranges:
     print('Setting motor to {}%.'.format(motor_range))
@@ -49,13 +57,24 @@ for motor_range in motor_ranges:
     # set motor controller range
     motor.set_pwm(motor_range)
     time.sleep(1.0) # wait time after to allow time for equilibration
+    
+    # start a noise stare if requested
+    if args.gcp_noise_stare:
+        stare_length = args.noise_stare_duration / 60.0
+        os.system('ssh sptdaq@192.168.2.4 "source ~/.bash_profile; '
+                  '/home/sptdaq/code/gcp/build/bin/sptCommand host=control.spt '
+                  'command=\'schedule noise_stare_no_acquire_20170910.sch( {:.0f}m )\'"'.format(stare_length))
+    time.sleep(10)
 
-    # save housekeeping data
     daq = MMA8451DAQ('motor={:.1f}'.format(motor_range))
+    
+    # save housekeeping data
     hkdata[motor_range] = {'voltage': args.voltage * (motor_range / 100.),
-                           'file': daq.fname}
-    with open(datetime.utcnow().strftime('%Y%m%d_%H%M%S_sweep_housekeeping.pkl'), 'w') as f:
+                           'file': daq.fname,
+                           'time': datetime.utcnow()}
+    with open(hkfilename, 'wb') as f:
         pickle.dump(hkdata, f)
     
     # take data for a specified duration
     daq.run(args.duration)
+    time.sleep(1)
